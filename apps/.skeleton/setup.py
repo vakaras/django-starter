52b176a491788a41356cb10ee30e60329fa1e88a
sirex <sirexas@gmail.com>
2010-11-11 14:12:19 +0200

    Adjustments to meet PEP8 ant dome additions.

--- apps/.skeleton/setup.py
+++ apps/.skeleton/setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='$(name)',
-    version='0.1',
-    author='$(author)',
-    author_email='$(author)@localhost',
-    packages=find_packages(),
-    url='http://pypi.python.org/pypi/$(name)/',
-    license='Commercial',
-    description='$(name).',
-    long_description=open('README.txt').read(),
+    name = '$(name)',
+    version = '0.1',
+    author = '$(author)',
+    author_email = '$(author)@localhost',
+    packages = find_packages(),
+    namespace_packages = [],
+    install_requires = [],
+    url = 'http://pypi.python.org/pypi/$(name)/',
+    license = 'Commercial',
+    description = '$(name).',
+    long_description = open('README.txt').read(),
 )
 

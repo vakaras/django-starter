275fbc1ef92c7e6c6839df80ae9f832c7537a7ad
holms <none@none>
2011-02-18 13:00:08 +0000

    unix compatibility added, sed fixed (one wrong file path, doesnt work in unix when combining sed commands), setup.py commented because it doesn't work

--- startapp.sh
+++ startapp.sh
@@ -7,14 +7,16 @@ elif [ -e apps/$1 ] ; then
 else
     modulename=$(echo $1 | sed 's/-/_/g')
     bin/django startapp $modulename && \
-    cp --preserve=mode -r apps/.skeleton apps/$1 && \
-    mv project/$modulename apps/$1/$modulename && \
-    sed -i \
-        -e 's/$(name)/'$1'/g' \
-        -e 's/$(author)/'$USER'/g' \
-        -e 's/$(date)/'$(date +%Y-%m-%d)'/g' \
-            apps/$1/CHANGES.txt \
-            apps/$1/docs/conf.py \
-            apps/$1/docs/index.rst \
-            apps/$1/setup.py 
+    #cp --preserve=mode -r apps/.skeleton apps/$1 && \ <-- please don't be GNU fan respect unix family too
+    cp -R -p apps/.skeleton apps/$1 && \
+    mv project/$modulename apps/$1/$modulename
+   
+
+    sed -i -e 's/$(date)/'$(date +%Y-%m-%d)'/g' apps/$1/CHANGES.txt
+    sed -i -e 's/$(author)/'$USER'/g'           apps/$1/docs/conf.py
+    sed -i -e 's/$(name)/'$1'/g'                apps/$1/docs/conf.py
+    sed -i -e 's/$(date)/'$(date +%Y-%m-%d)'/g' apps/$1/docs/index.rst
+
+   # python apps/$1/setup.py <-- this setup file is unfinished and do not work
 fi
+

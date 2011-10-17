e8e3ff56aaacd884419216e06aeaca3a71049984
Marius Grigaitis <none@none>
2011-10-17 23:51:53 +0300

    Fixed bug #10 247:django-starter user$ ./startapp.sh myapp sed: -e: No such file or directory https://bitbucket.org/sirex/django-starter/issue/10/sed-error-osx

--- startapp.sh
+++ startapp.sh
@@ -9,13 +9,8 @@ else
     bin/django startapp $modulename && \
     cp -R -p apps/.skeleton apps/$1 && \
     mv project/$modulename apps/$1/$modulename && \
-    sed -i \
-        -e 's/$(name)/'$1'/g' \
-        -e 's/$(author)/'$USER'/g' \
-        -e 's/$(date)/'$(date +%Y-%m-%d)'/g' \
-            apps/$1/CHANGES.txt \
-            apps/$1/docs/conf.py \
-            apps/$1/docs/index.rst \
-            apps/$1/setup.py 
-
+    files='apps/'$1'/CHANGES.txt apps/'$1'/docs/conf.py apps/'$1'/docs/index.rst apps/'$1'/setup.py' && \
+    sed -i -e 's/$(name)/'$1'/g' $files && \
+    sed -i -e 's/$(author)/'$USER'/g' $files && \
+    sed -i -e 's/$(date)/'$(date +%Y-%m-%d)'/g' $files
 fi
